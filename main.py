8c8843559d0ed887a48ad1775e63fd737f477f68
Dima0913 <dimarovno2009@gmail.com>
2024-01-24 15:01:33 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# start = int(input("enter start of diapason: "))
+# end = int(input("enter end of diapason: "))
+# for i in range(start,end):
+#     if i%7 == 0:
+#         print(i)
+
+
+
+
+# start = int(input("enter start of diapason: "))
+# end = int(input("enter end of diapason: "))
+# for i in range(start,end):
+#     if i % 3 == 0:
+#         print("Fizz")
+#     if i % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(i)
+    
+    
+    
+# start = int(input("enter start of diapason: "))
+# end = int(input("enter end of diapason: "))
+# count = 0
+# for i in range(start,end):
+#     print(i)   
+# for i in range(end,start-1,-1):
+#     print(i)
+# for i in range(start,end):
+#     if i%7 == 0:
+#         print(f"all nums kratni 7: {i}")
+#     if i%5 == 0:
+#         count += 1
+#         print(f"cilcist nums cratnuh 5: {count}")
+
+        
+
+
+
+        
